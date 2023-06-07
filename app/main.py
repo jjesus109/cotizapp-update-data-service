44857ddd0edab29460916dc589fc28ea7f4a9540
jesus <jesjalher@gmail.com>
2023-06-06 23:07:31 -0600

    feat: :sparkles: remove function on terminal signal

--- app/main.py
+++ app/main.py
@@ -90,16 +90,6 @@ async def main():
                 f"Could not update element id: {data_got.id} in DB"
             )
         log.info("Element updated!")
-
-
-import time
-
-def on_exit(sig, func=None):
-    print("exit handler")
-    time.sleep(10)  # so you can see the message before program exits
-
-import signal
-signal.signal(signal.SIGTERM, on_exit)
     
 if __name__ == "__main__":
     asyncio.run(main())
